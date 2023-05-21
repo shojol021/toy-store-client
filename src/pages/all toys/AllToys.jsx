3314b80d4d6808ba65805e08cd15fb3a4e53d26f
shojol021 <shojol021@gmail.com>
2023-05-21 11:44:24 +0600

    added readme.md file

--- src/pages/all toys/AllToys.jsx
+++ src/pages/all toys/AllToys.jsx
@@ -6,11 +6,23 @@ import { useState } from "react";
 const AllToys = () => {
     useTitle("All Toys")
 
-    const toys = useLoaderData()
-    const [allToys, setAllToys] = useState(toys)
+    const allToys = useLoaderData()
+    const [show, setShow] = useState(false)
+    const handleLoadMore = () => {
+        setShow(!show)
+    }
 
     return (
         <div>
+            <div className="flex items-center justify-around">
+                <h2 className="text-center font-bold text-3xl text-cyan-500 mt-12 ml-[40%]">All Toys</h2>
+                <div className="form-control me-12 mt-12">
+                    <label className="input-group">
+                        <span>Search</span>
+                        <input type="text" placeholder="Search by toy name" className="input input-bordered" />
+                    </label>
+                </div>
+            </div>
             <div className="overflow-x-auto w-full p-12">
                 <table className="table table-zebra w-full">
                     {/* head */}
@@ -25,7 +37,7 @@ const AllToys = () => {
                         </tr>
                     </thead>
                     {
-                        allToys.slice(0, 20).map(toy => <AllToysCard
+                        allToys.slice(0, `${show ? allToys.length : 20}`).map(toy => <AllToysCard
                             key={toy._id}
                             toy={toy}
                         ></AllToysCard>)
@@ -34,7 +46,7 @@ const AllToys = () => {
 
                 </table>
                 <div className="text-center mt-5">
-                    <button className="btn">Load More</button>
+                    <button onClick={handleLoadMore} className="btn">{show ? 'See Less' : 'Load More'}</button>
                 </div>
             </div>
         </div>
